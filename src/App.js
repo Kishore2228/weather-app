fb905ca4fa8c6cf443ed49f3916779809c6ba9a4
Kishore2228 <kishoreks2228@gmail.com>
2025-08-06 16:56:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "65229d6bf4253d55a750ea34b01c228a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+ 
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>Weather App</h1>
+
+        
+        <div>
+          <input
+            type="text"
+            placeholder="Enter city/town..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+
+        
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            
+            <p>{weather.name}</p>
+
+            
+            <p>{weather.main.temp}°C</p>
+
+            
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
